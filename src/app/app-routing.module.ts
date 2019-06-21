5bd42e46164a1191893344746a19b9824763aed6
luvo1995 <rambeniwanda@gmail.com>
2019-06-21 06:57:16 -0700

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path: "", component:MenuComponent, children:[
+  {path: "", component:HomeComponent},  
+  {path: "about", component:AboutComponent}, 
+  {path: "contact", component:ContactComponent},
+  {path: "feedback", component:FeedbackComponent}
+  
+]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
